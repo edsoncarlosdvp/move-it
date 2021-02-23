ea15a6e938c2a00d2de51240cfc9cd0bd6dc0935
edsoncarlosdvp <edsoncarlos@equipenovainfo.com.br>
2021-02-22 22:42:41 -0300

    Dia 01 => Expirience Bar. Concluída! (correção na branch main)

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ExpirienceBar } from './components/ExpirienceBar';
+import './styles/global.css';
+
+export function App(){
+    return (
+        
+        <div className="container">
+            <div>
+                <ExpirienceBar />
+            </div>
+        </div>
+
+    )
+}
---
+++ src/components/ExpirienceBar.tsx
@@ -0,0 +1,14 @@
+export function ExpirienceBar(){
+    return (
+        <header className="expirience-bar">
+            <span>0XP</span>
+            <div>
+                <div style={{ width: '50%' }}/>
+
+                <span className="current-expirience" style={{ left: '50%' }}>   300 XP
+                </span>
+            </div>
+            <span>600XP</span>
+        </header>
+    )
+}
